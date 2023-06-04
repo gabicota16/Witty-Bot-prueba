82e64583832609dd9b0bf463f74d4d7b01c1391c
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-06-04 18:20:33 -0300

    Witty - Bot

--- plugins/_autodel_chatgpt.js
+++ plugins/_autodel_chatgpt.js
@@ -9,7 +9,7 @@ async function deleteInactiveUserData(m) {
 
   if (currentTime - lastUpdateTime > INACTIVITY_TIMEOUT_MS) {
     delete global.chatgpt.data.users[m.sender];
-    //console.log(`Datos del usuario ${m.sender} eliminados después de ${INACTIVITY_TIMEOUT_MS / 1000 / 60} minutos de inactividad.`);
+    //console.log(`Datos del usuario ${m.sender} Eliminados después de ${INACTIVITY_TIMEOUT_MS / 1000 / 60} minutos de inactividad.`);
   }
 }
 
